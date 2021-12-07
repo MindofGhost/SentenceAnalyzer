b7f70a3ffb992fa99919a44a346ae287492e3bcf
Dsafgi-hub <dsafgi105@yandex.ru>
2021-12-07 10:48:38 +0300

    #39 В возвращаемый результат добавлены знаки препинания. Исправлена ошибка, когда не возвращались слова с буквой "ё" и слова продублированные много раз.

--- backendSentenceAnalyzer/src/main/java/ru/maistudents/backendsentenceanalyzer/services/TextService.java
+++ backendSentenceAnalyzer/src/main/java/ru/maistudents/backendsentenceanalyzer/services/TextService.java
@@ -3,6 +3,7 @@ package ru.maistudents.backendsentenceanalyzer.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.maistudents.backendsentenceanalyzer.analyzer.RussianLuceneMorphology;
+import ru.maistudents.backendsentenceanalyzer.dto.OutputWordDTO;
 import ru.maistudents.backendsentenceanalyzer.entities.Role;
 import ru.maistudents.backendsentenceanalyzer.entities.Text;
 import ru.maistudents.backendsentenceanalyzer.entities.Word;
@@ -33,43 +34,53 @@ public class TextService {
 
     }
 
-    public Map<String, String> getOutputText(Text text) {
-        Map<String, String> integerRoleMap = new LinkedHashMap<>();
+    public List<OutputWordDTO> getOutputText(Text text) {
+        List<OutputWordDTO> wordDTOList = new LinkedList<>();
         for(Word word: wordRepository.findAllByParentTextOrderByPosition(text)) {
-            integerRoleMap.put(word.getValue(), word.getRole().getName());
+            wordDTOList.add(new OutputWordDTO(word.getValue(), word.getRole().getName()));
         }
-        return integerRoleMap;
+        return wordDTOList;
     }
 
     public void analyzeText(Text text) {
-        final String delimiters = "[, .:\"«»—\\n\\t\\r\\f]";
+        final String delimiters = "[\\s\\n\\t\\r\\f]";
         String[] words = text.getText().split(delimiters);
         int position = 1;
         for (String word : words) {
-            if (word.matches("[а-яА-Я-]+")) {
-                createWord(text, word.trim(), position++);
-            }
+            createWord(text, word.trim(), position++);
         }
         textRepository.save(text);
     }
 
     private void createWord(Text text, String value, int position) {
-        Word word = new Word();
-        word.setParentText(text);
-        word.setPosition(position);
-        word.setValue(value);
-        word.setRole(generateRole(value));
-        wordRepository.save(word);
+        if (value != null && !value.equals("")) {
+            Word word = new Word();
+            word.setParentText(text);
+            word.setPosition(position);
+            word.setValue(value);
+            word.setRole(generateRole(value));
+            wordRepository.save(word);
+        }
     }
 
-    private Role generateRole(String value) {
-        value = value.toLowerCase(Locale.ROOT);
-        for (String word : russianLuceneMorphology.getMorphInfo(value)) {
-            if (word.split("\\|")[0].equals(value)) {
+    private Role generateRole(final String value) {
+        String delimiters = "[-:;.,!?]";
+        String valueWithoutPunctuationMark = value.toLowerCase(Locale.ROOT).split(delimiters)[0];
+
+
+        if (value.charAt(0) == '\"'
+                || value.charAt(0) == '\''
+                || value.charAt(0) == '«') {
+            delimiters = "[\"'«]";
+            valueWithoutPunctuationMark = value.toLowerCase(Locale.ROOT).split(delimiters)[1];
+        }
+
+        for (String word : russianLuceneMorphology.getMorphInfo(valueWithoutPunctuationMark)) {
+            if (word.split("\\|")[0].equals(valueWithoutPunctuationMark)) {
                 return switchRole(word.split(" ")[1]);
             }
         }
-        return switchRole(russianLuceneMorphology.getMorphInfo(value).get(0).split(" ")[1]);
+        return switchRole(russianLuceneMorphology.getMorphInfo(valueWithoutPunctuationMark).get(0).split(" ")[1]);
     }
 
     private Role switchRole(String lemma){
